6f86e4c7a24135e05f3915d24f30140c6d4d7b1f
Jo <jkgriffin234@gmail.com>
2018-10-05 16:23:43 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "Happiness is a warm puppy",
+    };
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" className="App-header" >
+        <header>{this.state.value}</header>
+        {/* continuous integration and delivery platform  */}
+        <button type="button" id="button1" onClick={() => this.setState({value: 'Happiness is an excellent continuous integration and delivery platform!'})} style={{background: "blue", border: "none", color: "white", padding: "15px 32px", fontSize: "16px"}} >Button #42</button>
+
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './setUpTests.js';
+import {shallow} from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
+
+it('loads text after button is clicked', () => {
+  const buttonWrapper = shallow(<button/>)
+  const wrapper = shallow(<App/>)
+
+  const text = 'Happiness is an excellent continuous integration and delivery platform!';
+
+  wrapper.find('#button1').simulate('click');
+  expect(wrapper.state().value).toEqual(text);
 });
